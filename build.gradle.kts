b116452c791e3e1d06c8c6af01f9197e3677bf9d
yunyoung1819 <yunyoung1819@hanmail.net>
2025-05-14 18:41:14 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "info.coroutine"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // 코루틴 라이브러리
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/section1/code1/Code1-1.kt
@@ -0,0 +1,5 @@
+package section1.code1
+
+fun main() {
+
+}
